14134b3e2e9dd69c1999f213158d3a20d819e679
vinoth <vinoth.aroumougam@gmail.com>
2017-09-20 23:53:52 +0530

    [ADD]: decision making & exp function

---
+++ decision_making.py
@@ -0,0 +1,28 @@
+# if else Statement
+
+a = 10
+if a % 2 == 0:
+    print("Even number")
+else:
+    print("Odd number")
+
+#-------------------------------------------------------------------------------------------------------    
+# single statement suites
+b=10
+
+if b ==10: print("the value of b is %s" %b)
+
+#-------------------------------------------------------------------------------------------------------
+
+#nested statements
+
+c = 100
+
+if c < 10:
+    print("The value is less than 10")
+elif c <50:
+    print("The value is less than 50")
+else:
+    print("the value is %s" %c)
+    
+#-------------------------------------------------------------------------------------------------------
