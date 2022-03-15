769e525f326c19e2ff881e4be3666dda3e1f1a5a
vinothzeabalane <omprakash.aroumougam@gmail.com>
2022-03-15 19:31:06 +0530

    Jython call JavaClass

---
+++ javafile/Sample.java
@@ -0,0 +1,9 @@
+package foo.src.foo;
+public class sample {
+   public void hello() {
+      System.out.println("Hello World!");
+   }
+   public void hello(String name) {
+      System.out.printf("Hello %s!", name);
+   }
+}
---
+++ jython-file/importex.py
@@ -0,0 +1,6 @@
+import sys
+sys.path.append(r'C:\Users\ozeabalx\eclipse-workspace')
+from foo.src.foo import sample
+h = sample()
+h.hello()
+h.hello("TutorialsPoint")
